12f3be8dd218e8a3ba7d37f1f488814e372ba6bd
Dmitriy <goglev37@gmail.com>
2024-06-21 16:42:41 +0300

    localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
-import React, {useState} from "react";
+import React from "react";
 import "./App.css";
 import {Counter} from "./components/Counter";
 import {CounterRandom} from "./components/CounterRandom";
 
+
 function App() {
-
-    const maxCount: number = 5
-    const minCount: number = 0
-    let [counter, setCounter] = useState<number>(minCount)
-
-    const onClickCounterIncHandler = () => {
-        if (counter < maxCount) {
-            setCounter(++counter)
-        }
-    }
-    const onClickCounterResetHandler = () => {
-        setCounter(minCount)
-    }
-
-
     return (
         <div className="App">
-            <Counter counter={counter} onClickCounterIncHandler={onClickCounterIncHandler}
-                     onClickCounterResetHandler={onClickCounterResetHandler} maxCount={maxCount} minCount={minCount}/>
-            <CounterRandom/>
+            <Counter/> {/*обычный счетчик */}
+            {/*<CounterRandom/> /!*рандомный счетчик*!/*/}
         </div>
     );
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,17 +1,49 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Button} from "./Button";
 
 type CounterType = {
-    counter: number
-    onClickCounterIncHandler: () => void
-    onClickCounterResetHandler: () => void
-    minCount: number
-    maxCount: number
+    // counter:number
+    // onClickCounterIncHandler: () => void
+    // onClickCounterResetHandler: () => void
+    // minCount: number
+    // maxCount: number
 }
 
 export const Counter = (props: CounterType) => {
 
-    const {counter, onClickCounterIncHandler, onClickCounterResetHandler, minCount, maxCount} = props
+    const maxCount: number = 5
+    const minCount: number = 0
+    let [counter, setCounter] = useState<number>(minCount)
+
+    useEffect(() => {
+        localStorageGet()
+    }, []);
+
+
+    useEffect(() => {
+        localStorageSet()
+    }, [counter]);
+
+    const localStorageSet = () => {
+        localStorage.setItem("counterValue", JSON.stringify(counter))
+    }
+    const localStorageGet = () => {
+        let counterString = localStorage.getItem("counterValue")
+        if (counterString) {
+            let newCounter = JSON.parse(counterString)
+            setCounter(newCounter)
+        }
+    }
+
+    const onClickCounterIncHandler = () => {
+        if (counter < maxCount) {
+            setCounter(++counter)
+        }
+
+    }
+    const onClickCounterResetHandler = () => {
+        setCounter(minCount)
+    }
 
     return (
         <div className={"counter-wrapper"}>
