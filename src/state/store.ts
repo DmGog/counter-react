6ca6ed76674110d5955ca2be9fbcb542ef58d505
Dmitriy <goglev37@gmail.com>
2024-08-21 11:24:25 +0300

    localStorage в redux

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,19 +2,26 @@ import {combineReducers, legacy_createStore} from "redux";
 import {counterReducer} from "./counter-reducer";
 import {maxValueReducer} from "./maxValue-reducer";
 import {startValueReducer} from "./startValue-reducer";
+import {loadState, saveState} from "../utils/localstorage-utils";
 
-// объединяя reducer-ы с помощью combineReducers,
-// мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
     count: counterReducer,
     maxValue: maxValueReducer,
     startValue: startValueReducer
 })
-// непосредственно создаём store
-export const store = legacy_createStore(rootReducer);
-// определить автоматически тип всего объекта состояния
+
+
+export const store = legacy_createStore(rootReducer, loadState());
+
+store.subscribe(() => {
+    saveState({
+        count: store.getState().count,
+        maxValue: store.getState().maxValue,
+        startValue: store.getState().startValue
+    })
+})
+
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
-// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store
